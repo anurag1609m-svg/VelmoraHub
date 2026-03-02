2f22555b14116c736232317f24c3b5e56eaee8eb
AnuragSharma <anurag1609.m@gmail.com>
2026-03-02 15:01:25 +0530

    Update constructor problem

--- src/main/java/com/piacart/entity/User.java
+++ src/main/java/com/piacart/entity/User.java
@@ -5,12 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 
-public @Data class User {
+
+public  class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
