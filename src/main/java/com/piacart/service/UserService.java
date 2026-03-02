166d1068afbcc5596157b1e2781c9830f65fdec1
AnuragSharma <anurag1609.m@gmail.com>
2026-03-02 17:47:19 +0530

    jwt token process done

--- src/main/java/com/piacart/service/UserService.java
+++ src/main/java/com/piacart/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.piacart.config.SecurityConfig;
 import com.piacart.entity.User;
 import com.piacart.repository.UserRepository;
+import com.piacart.security.JwtUtil;
 
 @Service
 public class UserService  {
@@ -21,6 +22,21 @@ public class UserService  {
     	 
     	 u.setRole("User");
     	 return us.save(u);
+    }
+    
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public String login(String email, String password) {
+
+        User user = us.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!ps.matches(password, user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return jwtUtil.generateToken(user.getEmail(), user.getRole());
     }
 	
 }
