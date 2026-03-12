fbbb873f68c457fec0824a4fd1cd17f009ceb11d
AnuragSharma <anurag1609.m@gmail.com>
2026-03-12 23:43:00 +0530

    latest commit

--- src/main/java/com/piacart/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.piacart.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.piacart.entity.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Integer> {
-
-}
